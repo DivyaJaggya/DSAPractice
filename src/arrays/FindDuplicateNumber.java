e5e59793b5cb02d83e6828139b87645174a76b78
LAPTOP-H0JP8UJ9\divya <divyajaggya@gmail.com>
2022-03-01 14:00:25 -0500

    commit for the code written

---
+++ src/arrays/FindDuplicateNumber.java
@@ -0,0 +1,39 @@
+package arrays;
+
+//https://leetcode.com/problems/missing-number/
+
+public class FindDuplicateNumber {
+	 public static int duplicateNumber(int[] nums) {
+         for(int i=0;i<nums.length;i++){
+             while(nums[i]<=nums.length && nums[nums[i]-1]!=nums[i]){
+                 //swap the numbers
+                 swap(nums, i, nums[i]-1);
+             }
+         }
+         for(int i=0;i<nums.length;i++){
+             if(nums[i]!=i+1 && nums[nums[i]-1]==nums[i]){
+                 return nums[i];
+             }
+         }
+         return 0;
+     }
+  public static void swap(int[]nums, int i, int j){
+      if(i==j){
+          return;
+      }
+      /*nums[i] = nums[i]^nums[j];
+      nums[j] = nums[i]^nums[j];
+      nums[i] = nums[i]^nums[j];*/
+      nums[i] = nums[i]+nums[j];
+      nums[j] = nums[i]-nums[j];
+      nums[i] = nums[i]-nums[j];
+  }
+  
+  public static void main(String[] args) {
+	  
+	  int[] array= new int[] {4,2,3,1,4};
+	  int missingNum=duplicateNumber(array);
+      System.out.println("Duplicate Number is : " + missingNum);
+  }
+
+}
