f1d4fca1cab8b27134ac2763dae8894a60e99520
LAPTOP-H0JP8UJ9\divya <divyajaggya@gmail.com>
2022-03-15 00:19:18 -0400

    commit for the final solution

--- src/arrays/FindMultipliedNumberInLargeString.java
+++ src/arrays/FindMultipliedNumberInLargeString.java
@@ -13,8 +13,28 @@ public class FindMultipliedNumberInLargeString {
         int[] result = new int[resultLen];
         int powerFactor=0, carry=0, prod=0;
         int k=0;
-        for(int i=num2.length()-1;i<0;i++){
-            k=
+        for(int i=num2.length()-1;i>=0;i--){
+            k=resultLen-1-powerFactor;
+            int currentI= num2.charAt(i)-'0';
+            for(int j=num1.length()-1;j>=0;j--){
+                int currentJ=num1.charAt(j)-'0';
+                prod = (currentJ*currentI)+ carry+ result[k];
+                result[k]=prod%10;
+                carry=prod/10;
+                k--;
+            }
+            if(carry>0){
+                result[k]=carry;
+            }
+            carry=0;
+            powerFactor++;
+        }
+        int k1=0;
+        while(k1>=0 && result[k1]==0){
+           k1++;
+        }
+        for(;k1<=resultLen-1;k1++){
+            MultipliedString+=result[k1];
         }
         return MultipliedString;
     }
@@ -22,7 +42,7 @@ public class FindMultipliedNumberInLargeString {
   
   public static void main(String[] args) {
 	  
-	  String MultipliedString =multiply("2147483","0");
+	  String MultipliedString =multiply("234643653434","234643653434");
       System.out.println("Multiplied result in String   : " + MultipliedString);
   }
 
